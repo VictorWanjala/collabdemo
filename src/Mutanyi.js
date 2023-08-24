a27e376ec00d22a1189366dce4269dd8bc94be18
tomutanyi <tomutanyi@gmail.com>
2023-08-24 19:35:22 +0300

    commiting tom's changes at 1935H

---
+++ src/Mutanyi.js
@@ -0,0 +1,14 @@
+function Mutanyi(){
+    return(
+        <div>
+            <h4>Hey my name is Tom Omele Mutanyi, here's a little something about me:</h4>
+           <p>
+           Salutations! I am a passionate full stack developer on an 
+           exciting journey of mastering the realms of JavaScript and Python. 
+           With an insatiable curiosity for coding and a determination to craft seamless digital experiences,
+            I'm dedicated to honing my skills to create dynamic and robust web applications from front to back.
+           </p>
+        </div>
+    )
+}
+export default Mutanyi
