97c714f4856842142b2a3904d75f65a067cfb833
tomutanyi <tomutanyi@gmail.com>
2023-08-24 19:44:24 +0300

    added route to app.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ function App() {
       <nav>
         <Link to='/masibo'>Masibo</Link>
         <button><Link to='/victor'>Victor</Link></button>
+        <Link to='/Mutanyi'>Mutanyi</Link>
       </nav>
       <Routes>
         <Route path='/masibo' element={<Masibo/>}/>
         <Route path='/Victor' element={<Victor/>}/>
+        <Route path='/Mutanyi' element={<Victor/>}/>
       </Routes>
     </div>
   );
