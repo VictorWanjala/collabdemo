4c6f467f328a01f329d83102dc6b3f9d0325e09c
ASUS <you@example.com>
2023-08-24 18:44:46 +0300

    Add Mercy Component

---
+++ src/Mercy.js
@@ -0,0 +1,11 @@
+function Mercy(){
+    return(
+        <div>
+            <h4>A little about Mercy Muriithi</h4>
+           <p>
+           Hey, I'm Mercy Muriithi, a coding enthusiast and student. I have been learning JavaScrip, HTML and CSS and I've embraced coding's problem-solving magic. I thrive on turning concepts into code, so far i have loved making Web Applications. Beyond coding, I love puzzles and collaboration. Join me in exploring the endless tech possibilities!  
+           </p>
+        </div>
+    )
+}
+export default Mercy
