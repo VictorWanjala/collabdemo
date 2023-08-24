3a51312ad7bad72d39fbb05d43af02c4b763945b
williamNyongesa <marakawilliam@gmail.com>
2023-08-24 19:01:05 +0300

    add app.js and william component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
-
+import William from './William';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <button> <Link to="/william">WILLIAM</Link>  </button> 
+      </nav>
       <Routes>
-        <Route></Route>
+      <Route path='/william' element={<William/>}/>
       </Routes>
     </div>
   );
