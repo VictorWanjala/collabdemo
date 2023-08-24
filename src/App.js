5e6584e6444343074c7a5fa90c5a50a7aecfbe0c
Masibo <victor.wafula@student.moringaschool.com>
2023-08-24 18:15:50 +0300

    add Masibo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
+import Masibo from './Masibo';
 
 function App() {
   return (
     <div className="App">
+      <Link to='/masibo'>Masibo</Link>
       <Routes>
-        <Route></Route>
+        
+        <Route path='/masibo' element={<Masibo/>}/>
       </Routes>
     </div>
   );
